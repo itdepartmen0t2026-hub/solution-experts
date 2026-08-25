ac2ce0247a6ffe589b91891659701cdeca0e7609
itdepartmen0t2026-hub <itdepartmen0t2026@gmail.com>
2026-08-25 16:52:37 +0530

    requiremnts add

--- staticfiles/js/script.js
+++
@@ -1,6 +0,0 @@
-
-document.addEventListener('DOMContentLoaded',()=>{
- document.querySelectorAll('.year').forEach(e=>e.textContent=new Date().getFullYear());
- const path=location.pathname.replace(/\\/g,'/');
- document.querySelectorAll('.navbar a.nav-link').forEach(a=>{const href=a.getAttribute('href'); if(href && !href.startsWith('#') && path.endsWith(href.replace('../',''))) a.classList.add('active');});
-});
