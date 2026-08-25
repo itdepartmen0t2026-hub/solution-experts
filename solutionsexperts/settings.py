918d6327311a278c7d0e6dd32d2d6d4d634083ec
itdepartmen0t2026-hub <itdepartmen0t2026@gmail.com>
2026-08-25 17:44:56 +0530

    whitenoise added

--- solutionsexperts/settings.py
+++ solutionsexperts/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,11 +127,11 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STORAGES = {
-#     "staticfiles": {
-#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-#     },
-# }
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static',
