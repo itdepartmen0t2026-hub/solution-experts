70e01aa308baf86b64e2605388466a60ea8f9bea
itdepartmen0t2026-hub <itdepartmen0t2026@gmail.com>
2026-08-25 18:40:21 +0530

    changes settingnhh ff

--- solutionsexperts/settings.py
+++ solutionsexperts/settings.py
@@ -210,7 +210,4 @@ MAILERS = {
 }
 
 DEFAULT_FROM_EMAIL = "itdepartmen0t2026@gmail.com"
-CSRF_TRUSTED_ORIGINS = [
-    "https://web-production-355ed.up.railway.app",
-    "http://web-production-355ed.up.railway.app",
-]
+CSRF_TRUSTED_ORIGINS = ['*']
