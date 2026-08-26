4761c5e113399850934102031c8fb0a83e1f555e
itdepartmen0t2026-hub <itdepartmen0t2026@gmail.com>
2026-08-26 15:01:46 +0530

    again cahnges csrf ff

--- solutionsexperts/settings.py
+++ solutionsexperts/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2o6oq*fbtm#&f-c!2zp_za!=)f4(w@sj*ndbljn$co91-%z=u*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["web-production-fc214.up.railway.app",]
 
