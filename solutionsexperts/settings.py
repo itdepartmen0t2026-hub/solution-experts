e242f0cf54455d0261939abdf28f56837235652b
itdepartmen0t2026-hub <itdepartmen0t2026@gmail.com>
2026-08-26 14:41:52 +0530

    changes settingn changes

--- solutionsexperts/settings.py
+++ solutionsexperts/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2o6oq*fbtm#&f-c!2zp_za!=)f4(w@sj*ndbljn$co91-%z=u*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['web-production-355ed.up.railway.app']
 
 import os
 # Application definition
